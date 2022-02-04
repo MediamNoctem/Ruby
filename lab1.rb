c5cff515418b489042c195e835714031cc9f7256
MediamNoctem <romAnastasia79@yandex.ru>
2022-02-04 16:19:12 +0300

    Update lab1.rb

--- lab1.rb
+++ lab1.rb
@@ -3,5 +3,31 @@
 #puts "Hello world"
 
 # 2
+=begin
 n = ARGV[0]
 puts "Здравствуйте, #{n}!"
+=end
+
+# 3
+# var1
+puts "Введите название Вашего любимого языка."
+l = gets.chop
+
+if l.downcase == "ruby"
+	puts "Вы подлиза!"
+else
+	if l.downcase == "python"
+		puts "Ожидаемо!"
+	else
+		if l.downcase == "c#"
+		puts "Неплохо!"
+		else
+			if l.downcase == "c++"
+				puts "Хорошо!"
+			else 
+				puts "..."
+			end
+		end
+	puts "Скоро будет ruby."
+	end
+end
